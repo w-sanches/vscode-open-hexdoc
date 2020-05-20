639d3fa0d2beed1a9146fcd268bf13da3b2a6c9a
Paulo Henrique da Silva <paulo.silva1337@gmail.com>
2020-05-20 14:54:51 -0300

    Add support to elixir version.

--- src/extension.ts
+++ src/extension.ts
@@ -5,25 +5,43 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('open-hexdoc.openHexDoc', () => {
 		vscode.workspace.findFiles('mix.lock').then((files) => {
 			vscode.workspace.openTextDocument(files[0]).then((document) => {
-				const libsAndVersions = document
-					.getText()
-					.split('\n')
-					.slice(1, -1)
-					.map(toLibAndVersion)
-					.filter((string) => string !== '');
-				vscode.window.showQuickPick(libsAndVersions, { canPickMany: false })
-					.then((pick) => {
-						if (pick) {
-							const [lib, version] = pick.split(":");
-							openDocs(lib, version);
-						}
-					});
+				findElixirVersion((elixirVersion: string) => {
+					const libsAndVersions = document
+						.getText()
+						.split('\n')
+						.slice(1, -1)
+						.map(toLibAndVersion)
+						.filter((string: string) => string !== '');
+
+					libsAndVersions.unshift(elixirVersion);
+
+					vscode.window.showQuickPick(libsAndVersions, { canPickMany: false })
+						.then((pick) => {
+							if (pick) {
+								const [lib, version] = pick.split(":");
+								openDocs(lib, version);
+							}
+						});
+				});
 			});
 		});
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+function findElixirVersion(callback: Function) {
+	const { spawn } = require('child_process');
+	const elixirVersion = spawn('elixir', ['-v']);
+
+	elixirVersion.stdout.on('data', (data: string) => {
+		const match = /Elixir ([0-9]+\.[0-9]+\.[0-9]+)/g.exec(data);
+		const version = match ? `elixir:${match.splice(1)}` : '';
+
+		callback(version);
+	});
+}
+
 function toLibAndVersion(string: string): string {
 	const match = /\s*"(.*)": {:.*, "([.\d]+)"/g.exec(string);
 	return match ? match.splice(1).join(':') : '';
